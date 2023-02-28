9f2cde2edc1ce03e2b552e361a3b4a1ce66afcc8
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-02-28 23:00:59 +0000

    test(backend): add remove student stats from dashboard tests

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/RemoveStatsFromTeacherDashboardTest.groovy
@@ -0,0 +1,64 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
+
+@DataJpaTest
+class RemoveStatsFromTeacherDashboardTest extends SpockTest {
+    def teacher
+    def teacherDashboard
+
+    def setup() {
+        createExternalCourseAndExecution()
+
+        teacher = new Teacher(USER_1_NAME, false)
+        userRepository.save(teacher)
+
+        teacherDashboard = new TeacherDashboard(externalCourseExecution, teacher)
+        teacherDashboardRepository.save(teacherDashboard)
+    }
+
+    def "remove student stats from teacher dashboard"() {
+        given: "student stats in teacher dashboard"
+        def studentStats = new StudentStats(externalCourseExecution, teacherDashboard)
+        studentStatsRepository.save(studentStats)
+
+        and:
+        def previousNumberStudentStats = teacherDashboard.getStudentStats().size()
+
+        when: "removing student stats from dashboard"
+        teacherDashboard.removeStudentStats(studentStats)
+
+        then: "it gets removed from dashboard"
+        teacherDashboard.getStudentStats().size() == previousNumberStudentStats - 1
+        !teacherDashboard.getStudentStats().contains(studentStats)
+    }
+
+    def "remove student stats from teacher dashboard it isn't part of"() {
+        given: "student stats in teacher dashboard"
+        def studentStats = new StudentStats()
+        studentStats.setCourseExecution(externalCourseExecution)
+        studentStatsRepository.save(studentStats)
+
+        and:
+        def previousNumberStudentStats = teacherDashboard.getStudentStats().size()
+
+        when: "removing student stats from dashboard"
+        teacherDashboard.removeStudentStats(studentStats)
+
+        then: "it gets removed from dashboard"
+        teacherDashboard.getStudentStats().size() == previousNumberStudentStats
+        def error = thrown(TutorException)
+        error.getErrorMessage() == ErrorMessage.STUDENT_STATS_NOT_FOUND
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration { }
+}
