17b12b162d11201a37290dca740a1e2a40411492
Diogo Cardoso <diogocardoso67@tecnico.ulisboa.pt>
2023-03-01 10:20:35 +0000

    refactor(backend): remove unused imports

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/RemoveStatsFromTeacherDashboardTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/RemoveStatsFromTeacherDashboardTest.groovy
@@ -6,8 +6,6 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
-import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Course
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
 
 @DataJpaTest
