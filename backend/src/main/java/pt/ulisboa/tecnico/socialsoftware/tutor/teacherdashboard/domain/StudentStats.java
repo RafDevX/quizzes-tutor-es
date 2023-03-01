b7584d60bca47d35a39c7db2bbe7d20ab5e11ac6
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-03-01 09:38:12 +0000

    feat(backend): implement updateNumMore75CorrectQuestions method
    Closes #16

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/StudentStats.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/StudentStats.java
@@ -1,10 +1,12 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain;
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuizAnswer;
 import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.Visitor;
 
 import javax.persistence.*;
+import java.util.stream.Collectors;
 
 @Entity
 public class StudentStats implements DomainEntity {
@@ -69,10 +71,34 @@ public class StudentStats implements DomainEntity {
 
     public void update() {
         this.updateNumStudents();
+        this.updateNumMore75CorrectQuestions();
     }
 
     private void updateNumStudents() {
         this.numStudents = this.courseExecution.getStudents().size();
+    }
+
+    private void updateNumMore75CorrectQuestions() {
+        this.numMore75CorrectQuestions = (int) this.courseExecution.getQuizzes()
+                .stream()
+                .flatMap(quiz -> quiz.getQuizAnswers().stream())
+                .collect(Collectors.groupingBy(
+                        QuizAnswer::getStudent
+                ))
+                .values()
+                .stream()
+                .map(answers -> {
+                    final long totalQuestions = answers.stream()
+                            .map(quizAnswer -> (long) quizAnswer.getQuestionAnswers().size())
+                            .reduce(0L, Long::sum);
+                    final long correctQuestions = answers.stream()
+                            .map(QuizAnswer::getNumberOfCorrectAnswers)
+                            .reduce(0L, Long::sum);
+
+                    return correctQuestions * 100 / totalQuestions;
+                })
+                .filter(correctRate -> correctRate >= 75)
+                .count();
     }
 
     public void accept(Visitor visitor) {
