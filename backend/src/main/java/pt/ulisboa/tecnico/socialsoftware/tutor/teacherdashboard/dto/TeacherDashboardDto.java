e4fe3d13d6f9a030d0662ef76c119789d1a4ec70
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-03-10 13:18:07 +0000

    feat(backend): extend TeacherDashboardDto to hold QuizStatsDTO objects
    Closes #64

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -2,9 +2,14 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TeacherDashboardDto {
     private Integer id;
     private Integer numberOfStudents;
+
+    private List<QuizStatsDto> quizStats;
 
     public TeacherDashboardDto() {
     }
@@ -13,6 +18,7 @@ public class TeacherDashboardDto {
         this.id = teacherDashboard.getId();
         // For the number of students, we consider only active students
         this.numberOfStudents = teacherDashboard.getCourseExecution().getNumberOfActiveStudents();
+        this.quizStats = teacherDashboard.getQuizStats().stream().map(QuizStatsDto::new).collect(Collectors.toList());
     }
 
     public Integer getId() {
@@ -31,11 +37,20 @@ public class TeacherDashboardDto {
         this.numberOfStudents = numberOfStudents;
     }
 
+    public List<QuizStatsDto> getQuizStats() {
+        return quizStats;
+    }
+
+    public void setQuizStats(List<QuizStatsDto> quizStats) {
+        this.quizStats = quizStats;
+    }
+
     @Override
     public String toString() {
         return "TeacherDashboardDto{" +
                 "id=" + id +
                 ", numberOfStudents=" + this.getNumberOfStudents() +
+                ", quizStats=" + this.getQuizStats() +
                 "}";
     }
 }
