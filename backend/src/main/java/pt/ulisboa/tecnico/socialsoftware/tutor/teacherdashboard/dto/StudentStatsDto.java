d0311904a5ecf9a15c938ad14a1932333d460519
Diogo Cardoso <diogocardoso67@tecnico.ulisboa.pt>
2023-03-13 12:38:05 +0000

    feat(backend): create StudentStatsDto class
    Closes #58

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/StudentStatsDto.java
@@ -0,0 +1,58 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.StudentStats;
+public class StudentStatsDto {
+    private int id;
+    private int numStudents;
+    private int numMore75CorrectQuestions;
+    private int numAtLeast3Quizzes;
+
+    public StudentStatsDto(StudentStats studentStats) {
+        this.id = studentStats.getId();
+        this.numStudents = studentStats.getNumStudents();
+        this.numMore75CorrectQuestions = studentStats.getNumMore75CorrectQuestions();
+        this.numAtLeast3Quizzes = studentStats.getNumAtLeast3Quizzes();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumStudents() {
+        return numStudents;
+    }
+
+    public void setNumStudents(int numStudents) {
+        this.numStudents = numStudents;
+    }
+
+    public int getNumMore75CorrectQuestions() {
+        return numMore75CorrectQuestions;
+    }
+
+    public void setNumMore75CorrectQuestions(int numMore75CorrectQuestions) {
+        this.numMore75CorrectQuestions = numMore75CorrectQuestions;
+    }
+
+    public int getNumAtLeast3Quizzes() {
+        return numAtLeast3Quizzes;
+    }
+
+    public void setNumAtLeast3Quizzes(int numAtLeast3Quizzes) {
+        this.numAtLeast3Quizzes = numAtLeast3Quizzes;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentStatsDto{" +
+                "id=" + id +
+                ", numStudents=" + numStudents +
+                ", numMore75CorrectQuestions=" + numMore75CorrectQuestions +
+                ", numAtLeast3Quizzes=" + numAtLeast3Quizzes +
+                '}';
+    }
+}
