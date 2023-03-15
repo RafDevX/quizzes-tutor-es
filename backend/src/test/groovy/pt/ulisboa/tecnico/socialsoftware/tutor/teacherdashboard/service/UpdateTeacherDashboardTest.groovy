b397eb6db60ee309d23614c5c746fbbab4f1294c
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-03-15 14:35:20 +0000

    test(backend): add service teacher dashboard update test with mocks
    Relates to #62

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/UpdateTeacherDashboardTest.groovy
@@ -0,0 +1,41 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.repository.TeacherDashboardRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.services.TeacherDashboardService
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
+
+@DataJpaTest
+class UpdateTeacherDashboardTest extends SpockTest {
+
+    def teacher
+
+    def setup() {
+        createExternalCourseAndExecution()
+
+        teacher = new Teacher(USER_1_NAME, false)
+        teacher.addCourse(externalCourseExecution)
+        userRepository.save(teacher)
+    }
+
+    def "update an existing teacher dashboard"() {
+        given: "a dashboard"
+        def dashboard = Mock(TeacherDashboard)
+        def repository = Mock(TeacherDashboardRepository)
+        repository.findById(1) >> Optional.of(dashboard)
+        def service = new TeacherDashboardService(teacherDashboardRepository: repository)
+
+        when: "the user updates the dashboard"
+        service.updateTeacherDashboard(1)
+
+        then: "the dashboard is updated"
+        1 * dashboard.update()
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
