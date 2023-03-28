64b90c22ff642fb14554c1085bf9386534c6ee33
Rafael Oliveira <rafael.serra.oliveira@tecnico.ulisboa.pt>
2023-03-28 11:45:11 +0100

    feat(frontend): extend TeacherDashboard model to hold QuestionStats
    Closes #108

--- frontend/src/models/teacherdashboard/TeacherDashboard.ts
+++ frontend/src/models/teacherdashboard/TeacherDashboard.ts
@@ -1,11 +1,18 @@
+import QuestionStats from './QuestionStats';
+
 export default class TeacherDashboard {
   id!: number;
   numberOfStudents!: number;
+
+  questionStats!: QuestionStats[];
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.numberOfStudents = jsonObj.numberOfStudents;
+      this.questionStats = jsonObj.questionStats.map(
+        (qs: QuestionStats) => new QuestionStats(qs)
+      );
     }
   }
 }
