63be9e12e153915c20d1b8bfebd2cd645b6b80a8
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-03-29 09:24:09 +0100

    feat(frontend): extend TeacherDashboard model to hold QuizStats
    Closes #106

--- frontend/src/models/teacherdashboard/TeacherDashboard.ts
+++ frontend/src/models/teacherdashboard/TeacherDashboard.ts
@@ -1,15 +1,20 @@
+import QuizStats from './QuizStats';
 import QuestionStats from './QuestionStats';
 
 export default class TeacherDashboard {
   id!: number;
   numberOfStudents!: number;
 
+  quizStats!: QuizStats[];
   questionStats!: QuestionStats[];
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.numberOfStudents = jsonObj.numberOfStudents;
+      this.quizStats = jsonObj.quizStats.map(
+        (stats: QuizStats) => new QuizStats(stats)
+      );
       this.questionStats = jsonObj.questionStats.map(
         (stats: QuestionStats) => new QuestionStats(stats)
       );
