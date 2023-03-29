a8ae880f3d2a56e6469af4c5e3fd95801dae45f9
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2023-03-29 10:30:15 +0100

    feat(frontend): create bar chart component for displaying teacher stats
    Closes #112

---
+++ frontend/src/components/BarChart.vue
@@ -0,0 +1,40 @@
+<template>
+  <Bar :chartOptions="chartOptions" :chartData="chartData" />
+</template>
+
+<script lang="ts">
+import { Bar } from 'vue-chartjs/legacy';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
